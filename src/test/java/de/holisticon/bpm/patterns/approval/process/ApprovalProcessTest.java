dfd9b6f25a6da1e933a599802f3f5a4985d941d0
Jo Ehm <jo.ehm@holisticon.de>
2017-08-25 14:53:13 +0200

    Get rid of camunda-bpm-needle

--- src/test/java/de/holisticon/bpm/patterns/approval/process/ApprovalProcessTest.java
+++ src/test/java/de/holisticon/bpm/patterns/approval/process/ApprovalProcessTest.java
@@ -1,16 +1,18 @@
 package de.holisticon.bpm.patterns.approval.process;
 
+import org.camunda.bpm.engine.ProcessEngineConfiguration;
 import org.camunda.bpm.engine.RuntimeService;
-import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.camunda.bpm.engine.impl.cfg.StandaloneInMemProcessEngineConfiguration;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
-import org.camunda.bpm.extension.needle.ProcessEngineNeedleRule;
-import org.camunda.bpm.extension.needle.ProcessEngineNeedleRuleBuilder;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.engine.test.assertions.bpmn.AbstractAssertions;
+import org.camunda.bpm.engine.test.mock.MockExpressionManager;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import javax.inject.Inject;
 import java.util.Map;
 
 import static org.camunda.bpm.engine.test.assertions.bpmn.AbstractAssertions.init;
@@ -21,22 +23,25 @@ import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareAssertions.as
  */
 public class ApprovalProcessTest {
 
+  private final ProcessEngineConfiguration configuration = new StandaloneInMemProcessEngineConfiguration() {{
+    this.jobExecutorActivate = false;
+    this.expressionManager = new MockExpressionManager();
+    this.databaseSchemaUpdate = ProcessEngineConfigurationImpl.DB_SCHEMA_UPDATE_DROP_CREATE;
+    this.isMetricsEnabled = false;
+  }};
+
   @Rule
-  public ProcessEngineNeedleRule rule = new ProcessEngineNeedleRuleBuilder(this).build();
+  public ProcessEngineRule rule = new ProcessEngineRule(configuration.buildProcessEngine());
 
-  @Inject
   RuntimeService runtimeService;
-
-  @Inject
-  TaskService taskService;
-
   private Driver driver;
   private ProcessInstance instance;
 
   @Before
   public void testSetup() {
 
-    init(rule.getProcessEngine());
+    AbstractAssertions.init(rule.getProcessEngine());
+    runtimeService = rule.getRuntimeService();
     driver = new Driver();
 
     // automatically mock all delegates
@@ -63,7 +68,6 @@ public class ApprovalProcessTest {
           .execute();
     }
   }
-
 
 
 }
